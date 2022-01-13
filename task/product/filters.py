d2735192f5526918cc300a7f82d6504367d7dfd4
alaa-22 <alaashaheen2205@gmail.com>
2022-01-13 16:13:02 +0200

    final edit

--- task/product/filters.py
+++ task/product/filters.py
@@ -3,6 +3,9 @@ from .models import ProductModel
 
 
 class ProductFilter(django_filters.FilterSet):
+    seller = \
+        django_filters.CharFilter(field_name='seller__username', lookup_expr='iexact')
+
     class Meta:
         model = ProductModel
         fields = ['seller']
--- task/product/urls.py
+++ task/product/urls.py
@@ -4,7 +4,6 @@ from .views import all_products, create_product,\
 
 urlpatterns = [
     path('', all_products, name="all product"),
-    path('filter/username/<str:name>/', product_filter, name="serch_product"),
     path('create/', create_product, name="create_product"),
     path('myproduct/', get_user_product, name="user_products"),
 
