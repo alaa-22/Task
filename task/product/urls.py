5ef5df337cccde16674e22e75c1b6720b812dee7
alaa-22 <alaashaheen2205@gmail.com>
2022-01-14 21:49:31 +0200

    add docker configration

--- task/product/urls.py
+++ task/product/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import all_products, create_product,\
-    product_filter, get_user_product
+from .views import all_products, create_product, get_user_product
 
 urlpatterns = [
     path('', all_products, name="all product"),
